0fae765cd035f8542611f58f092749d55ee34955
thegands <thegands@gmail.com>
2016-01-01 08:01:38 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, pos)
-  if board[pos] == "X" || board[pos] == "O"
+  if board[pos] == "X" || board[pos] == "O" #i felt this was a better way to address edge case scenarios than what the readme suggested
     true
   else
     false
